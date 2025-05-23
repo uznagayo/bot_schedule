455c2a890c2303ec6c74b75b7cdb707aedcc06bd
uznagayo <uznagato@gmail.com>
2025-05-23 21:54:41 +0300

    commit_23.05_2

--- schedule_bot/handlers/reminders.py
+++ schedule_bot/handlers/reminders.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-import aiogram 
+import aiogram
 from aiogram import types, Router, F, Bot
 import asyncio
 from utils.db import get_telegram_ids
@@ -13,6 +13,7 @@ reminders_router = Router()
 
 # if today.weekday() == 4 and today.hour() == 12:
 
+
 async def reminders(bot: Bot):
     # today = datetime.now()
     # today_str = today.strftime("%Y-%m-%d")
@@ -21,17 +22,20 @@ async def reminders(bot: Bot):
         # print(today)
         # print(today.weekday(), today.hour, today.minute)
         today_str = today.strftime("%Y-%m-%d")
-        if today.weekday() == 4 and today.hour == 15 and today.minute == 0:
+        if today.weekday() == 4 and today.hour == 16 and today.minute == 30:
             await select_schedule_rem(bot)
 
         await asyncio.sleep(60)
 
+
 async def select_schedule_rem(bot: Bot):
-    ids = get_telegram_ids('emploee')
+    ids = get_telegram_ids("employee")
     for id in ids:
         try:
-            await bot.send_message(chat_id=id, text='Пора выбрать смены, если еще не выбраны')
-            print('message send to', id)
+            await bot.send_message(
+                chat_id=id, text="Пора выбрать смены, если еще не выбраны"
+            )
+            print("message send to", id)
         except Exception as e:
             await print(id, e)
     # print('trying')
@@ -39,7 +43,5 @@ async def select_schedule_rem(bot: Bot):
     # try:
     #     await bot.send_message(chat_id=357434524, text='Пора выбрать смены, если еще не выбраны')
 
-
     # except Exception as e:
     #     print(e)
-
