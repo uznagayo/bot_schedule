571b5b0d5a01f2cdff5e5eb5cc63bff5b71e412d
uznagayo <43652362+uznagayo@users.noreply.github.com>
2025-06-16 16:22:07 +0300

    bugfix 16.6

--- schedule_bot/filters.py
+++ schedule_bot/filters.py
@@ -12,22 +12,33 @@ def is_user_registered(user_id: int) -> bool:
         cursor.execute("SELECT 1 FROM users WHERE telegram_id = ?", (user_id,))
         return cursor.fetchone() is not None
 
+
 class IsRegisteredMiddleware(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[Any, Dict[str, Any]], Awaitable[Any]],
         event: Message | CallbackQuery,
-        data: Dict[str, Any]
+        data: Dict[str, Any],
     ) -> Any:
         user_id = event.from_user.id
         if not is_user_registered(user_id):
             if isinstance(event, Message):
-                logger.info(event.from_user.id, event.from_user.first_name, "не зарегистрирован")
+                logger.info(
+                    f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован"
+                )
                 await event.answer("Ты не зарегистрирован в системе.")
-                await event.bot.send_message(chat_id=channel_id, text=f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован")
+                await event.bot.send_message(
+                    chat_id=channel_id,
+                    text=f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован",
+                )
             elif isinstance(event, CallbackQuery):
-                logger.info(event.from_user.id, event.from_user.first_name, "не зарегистрирован")
+                logger.infof(
+                    f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован"
+                )
                 await event.answer("Ты не зарегистрирован в системе.", show_alert=True)
-                await event.bot.send_message(chat_id=channel_id, text=f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован")
+                await event.bot.send_message(
+                    chat_id=channel_id,
+                    text=f"{event.from_user.id}, {event.from_user.first_name}, не зарегистрирован",
+                )
             return
         return await handler(event, data)
